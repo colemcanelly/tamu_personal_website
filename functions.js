6f9d074e515e0ad7320dd1b3f8c9ad0d27938c70
Cole McAnelly <cole.mcanelly3@gmail.com>
2023-01-31 12:35:56 -0600

    Created a header html section with style and a global style for the whole site. Also created functions.js to house all functions for the site, and added 2 functions to modularly add html to a page.

---
+++ functions.js
@@ -0,0 +1,20 @@
+// This function inserts html from a file into an element on the DOM
+let injectHTML = async (filePath,elem) => {
+    try {
+        const response = await fetch(filePath);
+        if (!response.ok) return;
+        const text = await response.text();
+        elem.innerHTML = text;
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+/*
+    This self-invoking function finds all `div` elements with `include` attributes
+    and inserts the specified file
+*/
+let injectAll = (() => {
+    document.querySelectorAll("div[include]")
+            .forEach( elem => { injectHTML(elem.getAttribute("include"),elem); } )
+})()
