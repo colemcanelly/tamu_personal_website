7c2213169d170e69337afd523258a1d98307371b
Cole McAnelly <cole.mcanelly3@gmail.com>
2023-02-07 21:07:45 -0600

    Implemented style toggle feature, and button to do so

--- scripts/functions.js
+++ scripts/functions.js
@@ -15,6 +15,29 @@ let injectHTML = async (filePath,elem) => {
     and inserts the specified file
 */
 let injectAll = (() => {
-    document.querySelectorAll("div[include]")
-            .forEach( elem => { injectHTML(elem.getAttribute("include"),elem); } )
-})()
+    document.querySelectorAll("div[include]").forEach(elem => {
+        injectHTML(elem.getAttribute("include"),elem);
+    });
+})();
+
+
+const STYLE1 = "style1";
+const STYLE2 = "style2";
+const STYLESHEET_KEY = "current_style_sheet";
+
+let setStyle = (style = "") => {
+    if (style === "") style = localStorage.getItem(STYLESHEET_KEY) || "style1";
+    document.querySelectorAll("link[href]").forEach(elem => {
+        let sheet = elem.getAttribute("href").split("/")[1];
+        console.log(sheet);
+        elem.setAttribute("href", style + '/' + sheet);
+    });
+}
+
+let styleToggle = () => {
+    const style = document.querySelector("link[href]").getAttribute("href").includes(STYLE1) ? STYLE2 : STYLE1;
+    setStyle(style);
+    localStorage.setItem(STYLESHEET_KEY, style);
+}
+
+window.onload = () => { setStyle(); }
